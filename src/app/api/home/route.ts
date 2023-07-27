096761634cc6069f28baced1cdbe03c9bca944f3
viluongEgitech <vi.luong@egitech.vn>
2023-07-27 13:24:01 +0700

    update code

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -4,7 +4,7 @@ import { HomeType } from './type'
 
 const prisma = new PrismaClient()
 
-// add project
+// add project hihi
 export const POST = async (request: any) => {
   const body: HomeType = await request.json()
 
